55fdce5151815bae62866836a67bc1ebe44f25e2
Sergi van Ravenswaay <contact@ikbensergi.com>
2017-11-14 12:01:34 +0100

    login system working

---
+++ Project/Assets/Scripts/Login/AuthenticationManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+[Serializable]
+public class User
+{
+    public bool success;
+    public string error;
+    public string email;
+}
+public class AuthenticationManager : MonoBehaviour {
+    public GameObject mainMenu;
+
+    public GameObject fieldEmailAddress;
+    public GameObject fieldPassword;
+    public GameObject fieldReenterPassword;
+
+    public Text textEmail;
+    public Text textPassword;
+    public Text ReenterPassword;
+
+    public bool ShowRegister = false;
+    public Text RegisterText;
+
+    public Text Login_Feedback;
+
+    WWWForm form;
+	// Use this for initialization
+	void Start () {
+        Login_Feedback.text = "";
+
+    }
+
+    public void LoginButtonTapped()
+    {
+        Login_Feedback.text = "Logging in...";
+        StartCoroutine("RequestLogin");
+    }
+    public IEnumerator RequestLogin()
+    {
+        string email = textEmail.text;
+        string password = textPassword.GetComponentInParent<InputField>().text;
+        form = new WWWForm();
+        form.AddField("usernamePost", email);
+        form.AddField("passwordPost", password);
+
+        WWW w = new WWW("http://81.169.177.181/UIB/action_login.php", form);
+
+        yield return w;
+        Login_Feedback.color = Color.black;
+        if (string.IsNullOrEmpty(w.error))
+        {
+
+            if (w.text.ToLower().Contains("invalid email or password"))
+            {
+                Login_Feedback.text = "Invalid email or password";
+                
+            }
+            else
+            {
+                Login_Feedback.text = "Login Successful";
+                App_Manager.instance.SetUsername(email);
+                SceneManager.LoadScene("Home", LoadSceneMode.Single);
+            }
+            }
+            else
+            {
+                Login_Feedback.text = "An Error Occured";
+            }
+        }
+}
---
+++ Project/Assets/Scripts/Manager/App_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class App_Manager : MonoBehaviour {
+    public static App_Manager instance;
+
+    [Header("Username of logged in User")]
+    [SerializeField]
+    private string username;
+
+    // Makes sure the App_Manager does not get destroyed & Singleton 
+    void Awake()
+    {
+        if (instance != null)
+            Debug.LogError("More than one App Manager in the scene");
+        else
+            instance = this;
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    // Set username from the Authentication Mgr
+    public void SetUsername (string _username) {
+        username = _username;
+	}
+    // Get username
+    public string GetUsername
+    {
+        get {
+            return username;
+        }
+    }
+
+    // Log out the user
+    public void LogOutUser()
+    {
+       SetUsername("");
+       SceneManager.LoadScene("Log_In", LoadSceneMode.Single);
+    }
+}
