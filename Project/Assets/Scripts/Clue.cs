cdb698dc662b94815abb60f82aada662eb44d894
shikariix <sarahsteenhuis@live.nl>
2017-11-20 10:43:04 +0100

    Added Vuforia files and AR scene.

---
+++ Project/Assets/Scripts/Clue.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Clues need to:
+ * Always face the camera
+ * Be clickable to inspect
+ * Each have their own text when inspected
+ */
+public class Clue : MonoBehaviour {
+    public string clueText;
+    public Text targetText;
+    public Transform target;
+    public Canvas canvas;
+
+	void Update () {
+        //Make sure clue is always facing camera (target)
+        transform.LookAt(target);
+    }
+
+    void OnMouseDown() {
+        canvas.gameObject.SetActive(true);
+        gameObject.SetActive(false);
+        targetText.text = clueText;
+    }
+}
