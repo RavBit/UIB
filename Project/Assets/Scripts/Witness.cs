12da7de41d4049f9db28b83631b6ee0a887ee689
Sergi van Ravenswaay <svanravenswaay@perspectiveworks.nl>
2017-12-04 23:49:58 +0100

    witness, quests and dialogs ported over to faster json loading

--- Project/Assets/Scripts/Witness.cs
+++ Project/Assets/Scripts/Witness.cs
@@ -1,4 +1,8 @@
-﻿
+﻿using System.Collections.Generic;
+using System;
 [System.Serializable]
 public class Witness {
+    public int id;
+    public string name;
+    public List<Quest_BaseDialog> dialogs;
 }
