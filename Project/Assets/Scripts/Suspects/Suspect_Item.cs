dd86430413770c9581c8f9ef7a6013b945dae3c5
Sergi van Ravenswaay <contact@ikbensergi.com>
2017-12-07 17:01:48 +0100

    fixed quest dialog

--- Project/Assets/Scripts/Suspects/Suspect_Item.cs
+++ Project/Assets/Scripts/Suspects/Suspect_Item.cs
@@ -1,16 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.UI;
 public class Suspect_Item : MonoBehaviour {
+    public Text Name;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //ADD CLOTHING GENERATOR AND SUCH
 }
