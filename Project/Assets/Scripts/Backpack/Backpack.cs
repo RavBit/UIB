84f16fb4efb21ba26eccfcb551397878dbd37764
Sergi van Ravenswaay <contact@ikbensergi.com>
2017-12-21 11:21:52 +0100

    added working backpack

---
+++ Project/Assets/Scripts/Backpack/Backpack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backpack : MonoBehaviour {
+    public GameObject CluePrefab;
+    public Transform ClueContainer;
+    public void Display() {
+        List<Quest_Clues> QC = Event_Manager.Get_Clues();
+        foreach(Quest_Clues clue in QC) {
+            if(clue.found == 1) {
+                GameObject cp = Instantiate(CluePrefab, Vector3.zero, Quaternion.identity);
+                cp.GetComponent<Backpack_ClueDisplay>().clue.text = clue.clue;
+                cp.transform.parent = ClueContainer;
+            }
+        }
+    }
+}
